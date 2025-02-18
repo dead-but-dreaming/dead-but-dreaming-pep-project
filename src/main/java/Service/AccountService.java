e670a263af96aebf1d065c5991ab025529aae3d4
Rob Bird <allyoursanity@gmail.com>
2025-02-18 14:56:05 +0000

    Service constructors added.

--- src/main/java/Service/AccountService.java
+++ src/main/java/Service/AccountService.java
@@ -4,5 +4,16 @@ import Model.Account;
 import DAO.AccountDAO;
 
 public class AccountService {
-    
+    private AccountDAO accountDAO;
+
+    // constructor
+    public AccountService(){
+        accountDAO = new AccountDAO();
+    }
+
+    // constructor with existing DAO
+    public AccountService(AccountDAO accountDAO){
+        this.accountDAO = accountDAO;
+    }
+
 }
--- src/main/java/Service/MessageService.java
+++ src/main/java/Service/MessageService.java
@@ -4,5 +4,16 @@ import Model.Message;
 import DAO.MessageDAO;
 
 public class MessageService {
+    private MessageDAO messageDAO;
+
+    // constructor
+    public MessageService(){
+        messageDAO = new MessageDAO();
+    }
+
+    // constructor with existing DAO
+    public MessageService(MessageDAO messageDAO){
+        this.messageDAO = messageDAO;
+    }
     
 }
